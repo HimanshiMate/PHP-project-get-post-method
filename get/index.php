39874d3ed052d4a90f12e69ee77fb00558f2636a
Himanshi Mate <matemanu725@gmail.com>
2024-08-04 23:17:04 +0530

    import

---
+++ get/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body bgcolor="skyblue">
+
+  <h1>Application form</h1>
+  <form method="get" action="save.php">
+    enter name: <input type="text" name="nm">
+    enter city: <input type="text" name="city">
+    <input type="submit" value="save">
+  </form>
+
+</body>
+
+</html>
---
+++ get/save.php
@@ -0,0 +1,18 @@
+<?php
+  $name=$_GET["nm"];
+  $city=$_GET["city"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Name: $name ";
+    echo "city: $city" ;
+    ?>
+</body>
+</html>
